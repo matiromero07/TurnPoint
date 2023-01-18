58a69db17adcf6a0c3bffbe724f728d31269b4d8
matiromero07 <matiromero011@gmail.com>
2023-01-18 20:28:34 -0300

    first commit

---
+++ src/main/java/com/turnPoint/pil/PilApplication.java
@@ -0,0 +1,13 @@
+package com.turnPoint.pil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnPoint/pil/PilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnPoint.pil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
