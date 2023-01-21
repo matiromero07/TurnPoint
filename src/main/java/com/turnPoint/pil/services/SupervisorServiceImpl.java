dad249905799dccf23d978733bfafc04161208fe
Paz <di.pazcoll@gmail.com>
2023-01-21 01:08:23 -0300

    Supervisor Service Impl ready

--- src/main/java/com/turnPoint/pil/services/SupervisorServiceImpl.java
+++ src/main/java/com/turnPoint/pil/services/SupervisorServiceImpl.java
@@ -3,10 +3,12 @@ package com.turnPoint.pil.services;
 import com.turnPoint.pil.model.Supervisor;
 import com.turnPoint.pil.repository.SupervisorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class SupervisorServiceImpl implements SupervisorService {
 
     @Autowired
