5b13f33c4f4a11f032c10646a80ef3807133f069
Paz <di.pazcoll@gmail.com>
2023-01-21 00:25:38 -0300

    SupervisorRepository added

---
+++ src/main/java/com/turnPoint/pil/repository/SupervisorRepository.java
@@ -0,0 +1,7 @@
+package com.turnPoint.pil.repository;
+
+import com.turnPoint.pil.model.Supervisor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SupervisorRepository extends CrudRepository<Supervisor, Long> {
+}
