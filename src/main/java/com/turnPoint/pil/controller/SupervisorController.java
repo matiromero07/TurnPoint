12a63c1f963d843f071e4a63d6e4d093740f740a
Lucas Heredia <lucasheredia5475@gmail.com>
2023-01-23 20:49:41 -0300

    add the method PutMapping on SupervisorController to be able to edit a supervisor

--- src/main/java/com/turnPoint/pil/controller/SupervisorController.java
+++ src/main/java/com/turnPoint/pil/controller/SupervisorController.java
@@ -2,6 +2,8 @@ package com.turnPoint.pil.controller;
 
 import com.turnPoint.pil.model.Supervisor;
 import com.turnPoint.pil.model.SupervisorWrapper;
+import com.turnPoint.pil.model.Zone;
+import com.turnPoint.pil.model.ZoneWrapper;
 import com.turnPoint.pil.services.SupervisorService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -34,6 +36,24 @@ public class SupervisorController {
         }
         else{
             return SupervisorWrapper.builder().supervisor(listSupervisor.get()).message("Supervisor found").build();
+        }
+    }
+
+    @PutMapping("/supervisor/{id}")
+    public SupervisorWrapper editSupervisor(@RequestBody Supervisor supervisor, @PathVariable Long id) {
+        var listSupervisor = supervisorService.listSupervisorById(id);
+
+        if (listSupervisor.isEmpty()) {
+            return SupervisorWrapper.builder().supervisor(null).message("No se encontró el supervisor").build();
+        } else {
+
+            supervisor.setId(id);
+            supervisor.setName(supervisor.getName());
+            supervisor.setSurname(supervisor.getSurname());
+            supervisor.setPhoneNumber(supervisor.getPhoneNumber());
+
+            var savedSupervisor = supervisorService.saveSupervisor(supervisor);
+            return SupervisorWrapper.builder().supervisor(savedSupervisor).message("Supervisor editado").build();
         }
     }
 }
