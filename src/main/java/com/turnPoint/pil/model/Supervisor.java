8bb5c632dc59fe1ba1533d82f1a8160ae5e0f909
matiromero07 <matiromero011@gmail.com>
2023-01-26 20:07:54 -0300

    add the annotation @NoArgsConstructor on Supervisor.java class

--- src/main/java/com/turnPoint/pil/model/Supervisor.java
+++ src/main/java/com/turnPoint/pil/model/Supervisor.java
@@ -1,10 +1,12 @@
 package com.turnPoint.pil.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
+@NoArgsConstructor
 @Entity
 @Getter
 @Setter
